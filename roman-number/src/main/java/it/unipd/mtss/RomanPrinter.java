cc0c98371b555abf06e6d728d73201183fa7efea
clumsycatgirl <282945455+eric-michelon-unipd@users.noreply.github.com>
2026-05-08 21:23:11 +0200

    inizializzazione progetto Maven con classi stub
    - Generata la struttura del progetto
    - Aggiunta classe stub IntegerToRoman
    - Aggiunta classe stub RomanPrinter
    - Verificata la compilazione con mvn compile
    
    Closes #2

---
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////////
+// [ERIC] [MICHELON] [2147986]
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+public class RomanPrinter {
+	public static String print(int num) {
+		return printAsciiArt(IntegerToRoman.convert(num));
+	}
+
+	private static String printAsciiArt(String romanNumber) {
+		// TODO
+		return null;
+	}
+}
