206d4ebd7e516bd6f93aaf614ad9b35f5773c995
michelon-eric <michelon.eric.2005@gmail.com>
2026-05-09 18:19:46 +0200

    implementazione numeri fino a 1000

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -5,15 +5,15 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     private static final int[] VALUES = {
-            500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
+            1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
     };
 
     private static final String[] SYMBOLS = {
-            "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+            "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
     };
 
     public static String convert(int number) {
-        if (number < 1 || number > 500) {
+        if (number < 1 || number > 1000) {
             return null;
         }
 
