eeef81012cc59d5645bac7631f60e6cc05816ef4
eric-michelon-unipd <282945455+eric-michelon-unipd@users.noreply.github.com>
2026-05-09 16:51:57 +0200

    checkstyle fix

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -4,27 +4,27 @@
 package it.unipd.mtss;
 
 public class IntegerToRoman {
-	private static final int[] VALUES = {
-		10, 9, 5, 4, 1
-	};
+    private static final int[] VALUES = {
+        10, 9, 5, 4, 1
+    };
 
-	private static final String[] SYMBOLS = {
-		"X", "IX", "V", "IV", "I"
-	};
+    private static final String[] SYMBOLS = {
+        "X", "IX", "V", "IV", "I"
+    };
 
-	public static String convert(int number) {
-		if (number < 1 || number > 10) {
-			return null;
-		}
+    public static String convert(int number) {
+        if (number < 1 || number > 10) {
+            return null;
+        }
 
-		StringBuilder result = new StringBuilder();
-		for (int i = 0; i < VALUES.length; i++) {
-			while (number >= VALUES[i]) {
-				result.append(SYMBOLS[i]);
-				number -= VALUES[i];
-			}
-		}
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < VALUES.length; i++) {
+            while (number >= VALUES[i]) {
+                result.append(SYMBOLS[i]);
+                number -= VALUES[i];
+            }
+        }
 
-		return result.toString();
-	}
+        return result.toString();
+    }
 }
